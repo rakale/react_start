b0254186dcf0d24a6f27e8da5b2be01304d51d4a
Raiko Kalev <rakale@ttu.ee>
2021-08-11 15:41:05 +0300

    Section 5 complete, added chartbar

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,43 +1,32 @@
-import ExpenseItem from './ExpenseItem';
-import './Expenses.css';
-import Card from '../UI/Card';
-import React,{useState} from 'react';
-import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from "./ExpensesList";
+import "./Expenses.css";
+import Card from "../UI/Card";
+import React, { useState } from "react";
+import ExpensesFilter from "./ExpensesFilter";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
-    const [filteredYear, setFilteredYear] = useState('2020');
-    const filterChangeHandler = selectedYear => {
-        setFilteredYear(selectedYear);
-    };
-    return (
-        <div>
-    <Card className="expenses">
-        <ExpensesFilter selected ={filteredYear} onChangeFilter ={filterChangeHandler}/>
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
 
+  const filterExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
 
-        <ExpenseItem
-            title={props.items[0].title}
-            amount={props.items[0].amount}
-            date={props.items[0].date}
-        ></ExpenseItem>
-        <ExpenseItem
-            title={props.items[1].title}
-            amount={props.items[1].amount}   
-            date={props.items[1].date}
-        ></ExpenseItem>
-        <ExpenseItem
-            title={props.items[2].title}
-            amount={props.items[2].amount}
-            date={props.items[2].date}
-        ></ExpenseItem>
-        <ExpenseItem
-            title={props.items[3].title}
-            amount={props.items[3].amount}
-            date={props.items[3].date}
-        ></ExpenseItem>
-
-    </Card>
-        </div>  )    
-}
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesChart expenses={filterExpenses} />
+        <ExpensesList items={filterExpenses} />
+      </Card>
+    </div>
+  );
+};
 
 export default Expenses;
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -24,7 +24,7 @@ const ExpenseForm = (props) => {
 
         const expenseData = {
             title: enteredTitle,
-            amount: enteredAmount,
+            amount: +enteredAmount,
             date: new Date(enteredDate)
         };
 
@@ -62,6 +62,7 @@ const ExpenseForm = (props) => {
             </div>
             <div className="new-expense__actions">
                 <button type="submit">Add expense</button>
+                <button type="button" onClick={props.onCancel}>Cancel</button>
             </div>
         </form>
     );
